243b6ffae4b5e9d0c6292e97293b0d806cc487b5
Lord-Scrubington-II (Zane W.) <zane.z.wang@gmail.com>
2021-11-21 23:42:56 -0800

    formatting fixes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,7 +2,10 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 
-function createWindow () {
+/**
+ * Initializes the Window.
+ */
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1280,
@@ -25,19 +28,19 @@ function createWindow () {
 app.whenReady().then(() => {
   createWindow();
   
-  app.on('activate', function () {
+  app.on('activate', function() {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  })
-})
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') app.quit();
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
