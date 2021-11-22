3690ec54b08c606c436ad6c2dc5d46a49bc1d7ef
Haxoc <haxoc@protonmail.com>
2021-11-21 19:26:18 -0800

    Added eslint google style, changed install script

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 pip install html5validator
 bash <(curl https://raw.githubusercontent.com/rhysd/actionlint/main/scripts/download-actionlint.bash)
 
-npm install htmlhint --save-dev
-npm install eslint@6.8.0 --save-dev
-npm install jscpd  --save-dev
-npm install stylelint stylelint-config-standard --save-devs
+npm install --save-dev htmlhint 
+npm install --save-dev eslint@6.8.0 eslint-config-google  
+npm install --save-dev jscpd  
+npm install --save-dev stylelint stylelint-config-standard 
 
 touch .eslintrc.json
 mv .eslintrc.json .esintrc1.json
