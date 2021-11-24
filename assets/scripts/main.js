95b95afe01fbcc792057b04e13d1dae85ba8b837
Haxoc <haxoc@protonmail.com>
2021-11-23 17:04:40 -0800

    removed newline

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -52,7 +52,6 @@ app.on('window-all-closed', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-
 /**
  * Loads all of the json files in the "./assets/recipes" folder
  * into our data structures
