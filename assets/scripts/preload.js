9b3b23b60ac29680ee51dc25f47d8a85712f4718
CJ-314 <cyuan@ucsd.edu>
2021-11-24 21:07:13 -0800

    add remove

--- assets/scripts/preload.js
+++ assets/scripts/preload.js
@@ -2,4 +2,5 @@ const {contextBridge, ipcRenderer} = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
   addRecipe: (recipeData, recipeName) => ipcRenderer.sendSync('ADD', recipeData, recipeName),
+  removeRecipe: (recipeName) => ipcRenderer.sendSync('DELETE', recipeName),
 });
