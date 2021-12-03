d8d3e91360232ec0c261ba15159602ac9688694a
Ceejay <59652011+314-Y@users.noreply.github.com>
2021-12-03 10:59:18 -0800

    fix format

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,8 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const IOSystem = require('./IOSystem');
-const { ipcMain } = require('electron');
+const {ipcMain} = require('electron');
 
 // directory with recipes
 const RECIPES_DIR = path.join(__dirname, '../recipes/');
@@ -14,43 +14,43 @@ const IMAGES_DIR = path.join(__dirname, '../recipes/images/');
  * Initializes the Window.
  */
 function createWindow() {
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        icon: '../images/panda.ico',
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-        },
-    });
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    icon: '../images/panda.ico',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
 
-    // and load the index.html of the app.
-    mainWindow.loadFile('index.html');
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html');
 
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    createWindow();
+  createWindow();
 
-    app.on('activate', function() {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 
-    cacheRecipesFromDisk();
+  cacheRecipesFromDisk();
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function() {
-    if (process.platform !== 'darwin') app.quit();
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
 });
 
 // In this file you can include the rest of your app's specific main process
@@ -61,22 +61,22 @@ app.on('window-all-closed', function() {
  * into our data structures
  */
 function cacheRecipesFromDisk() {
-    // READ: retrieve all entries from disk complete
-    const readDict = IOSystem.scanFiles(RECIPES_DIR);
+  // READ: retrieve all entries from disk complete
+  const readDict = IOSystem.scanFiles(RECIPES_DIR);
 
-    for (const fileObj of readDict) {
-        const filePath = fileObj.path;
-        const fileData = fileObj.data;
-        const fileAsJSON = JSON.parse(fileData);
+  for (const fileObj of readDict) {
+    const filePath = fileObj.path;
+    const fileData = fileObj.data;
+    const fileAsJSON = JSON.parse(fileData);
 
-        IOSystem.indexRecipe(fileAsJSON.name, fileAsJSON);
-        IOSystem.indexFile(fileAsJSON.name, filePath);
-    }
-    console.log(IOSystem.recipesDict);
-    console.log(IOSystem.filesDict);
+    IOSystem.indexRecipe(fileAsJSON.name, fileAsJSON);
+    IOSystem.indexFile(fileAsJSON.name, filePath);
+  }
+  console.log(IOSystem.recipesDict);
+  console.log(IOSystem.filesDict);
 }
 ipcMain.on('LOAD', (event) => {
-    event.returnValue(IOSystem.recipesDict);
+  event.returnValue(IOSystem.recipesDict);
 });
 
 /**
@@ -86,14 +86,14 @@ ipcMain.on('LOAD', (event) => {
  */
 
 ipcMain.on('ADD', (event, recipeData, recipeName) => {
-    try {
-        IOSystem.dumpJSON(recipeData, RECIPES_DIR, `${recipeName}.json`);
-        IOSystem.indexRecipe(recipeName, recipeData);
-        event.returnValue = 'SUCCESS';
-    } catch (err) {
-        console.error(err);
-        event.returnValue = 'FAILED';
-    }
+  try {
+    IOSystem.dumpJSON(recipeData, RECIPES_DIR, `${recipeName}.json`);
+    IOSystem.indexRecipe(recipeName, recipeData);
+    event.returnValue = 'SUCCESS';
+  } catch (err) {
+    console.error(err);
+    event.returnValue = 'FAILED';
+  }
 });
 
 /**
@@ -102,13 +102,13 @@ ipcMain.on('ADD', (event, recipeData, recipeName) => {
  * If success "SUCCESS" will be returned.
  */
 ipcMain.on('COPY_IMAGE', (event, imgPath, imgName) => {
-    try {
-        IOSystem.copyFile(imgPath, IMAGES_DIR, imgName);
-        event.returnValue = 'SUCCESS';
-    } catch (err) {
-        console.error(err);
-        event.returnValue = 'FAILED';
-    }
+  try {
+    IOSystem.copyFile(imgPath, IMAGES_DIR, imgName);
+    event.returnValue = 'SUCCESS';
+  } catch (err) {
+    console.error(err);
+    event.returnValue = 'FAILED';
+  }
 });
 
 /**
@@ -117,23 +117,23 @@ ipcMain.on('COPY_IMAGE', (event, imgPath, imgName) => {
  *  If success "SUCCESS" will be returned.
  */
 ipcMain.on('DELETE', (event, recipeName) => {
-    try {
-        IOSystem.eraseFileAt(RECIPES_DIR, `${recipeName}.json`);
-        delete IOSystem.recipesDict[recipeName];
-        event.returnValue = 'SUCCESS';
-    } catch (err) {
-        event.returnValue = 'FAILED';
-    }
+  try {
+    IOSystem.eraseFileAt(RECIPES_DIR, `${recipeName}.json`);
+    delete IOSystem.recipesDict[recipeName];
+    event.returnValue = 'SUCCESS';
+  } catch (err) {
+    event.returnValue = 'FAILED';
+  }
 });
 
 ipcMain.on('ACQUIRE', (event, recipeName) => {
-    let fileDir = RECIPES_DIR + `${recipeName}.json`;
-    event.returnValue = require(fileDir);
+  let fileDir = RECIPES_DIR + `${recipeName}.json`;
+  event.returnValue = require(fileDir);
 });
 
 /**
  * Acquire the dictionary of recipes
  */
 ipcMain.on('CACHEDICT', (event) => {
-    event.returnValue = IOSystem.recipesDict;
+  event.returnValue = IOSystem.recipesDict;
 });
