18450b60ccf13b3f51df26a8707577eb85445554
Haxoc <haxoc@protonmail.com>
2021-12-09 14:33:08 -0800

    Fix bash issue

--- ci/buildall.sh
+++ ci/buildall.sh
@@ -24,7 +24,7 @@ cp -r assets electron-build/assets/
 cp index.html electron-build/index.html
 cp ci/release-package.json electron-build/package.json
 
-cd electron-build
+cd electron-build || exit
 npm install
 cd ..
 npm run-script "build-${PLATFORM}"
